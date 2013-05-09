c0f5836871c7e036416db88ab265f6c3b62462a8
a <a@bortnikov.com>
2013-05-09 16:43:22 +0400

    Excersize 1.8. Newton's method for cube roots.

---
+++ 1.1/1.8.scm
@@ -0,0 +1,14 @@
+(define (cube-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cube-iter (improve guess x)
+		 x)))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+(define (cube-root x)
+  (cube-iter 1.0 x))
+
+(define (good-enough? guess x)
+  (< (abs (- (improve guess x) guess)) (* guess 0.001)))
