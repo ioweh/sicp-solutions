fbdfa3c7dff85bd60d17397c18e8d726b3d33974
a <a@bortnikov.com>
2013-07-05 02:07:45 +0400

    Excercise 1.45. A simple procedure for computing nth roots

---
+++ 1.3/1.45.scm
@@ -0,0 +1,87 @@
+
+(define (compose f g)
+  (lambda (y) (f (g y))))
+;Value: compose
+
+(define (inc x) (+ x 1))
+;Value: inc
+
+(define (repeated func n)
+  (define (iter i)
+    (if (< i n)
+	(compose func (iter (+ i 1)))
+	(lambda (x) (func x))))
+  (iter 1))
+;Value: repeated
+
+((repeated square 2) 5)
+;Value: 625
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+;Value: average-damp
+
+(define (average x y)
+  (/ (+ x y) 2))
+;Value: average
+
+((average-damp square) 10)
+;Value: 55
+
+(define tolerance 0.00001)
+;Value: tolerance
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+	  next
+	  (try next))))
+  (try first-guess))
+;Value: fixed-point
+
+(fixed-point cos 1.0)
+;Value: .7390822985224024
+
+(define (cube x) (* x x x))
+;Value: cube
+
+
+(define (nth-root x n)
+  (fixed-point ((repeated average-damp (log2 n)) (lambda (y) (/ x (exponent y (- n 1)))))
+	       1.0))
+;Value: nth-root
+
+
+(nth-root 16 68)
+;Value: 1.0416241748371353
+
+
+(define (exponent x n)
+  (define (iter i)
+    (if (< i n)
+	(* x (iter (+ i 1)))
+	x))
+  (iter 1))
+;Value: exponent
+
+
+(for square root 1 average-damp)
+(for 4th root 2 average-damps)
+(for 8th root 3 average-damps)
+(for 16th root 4 average-damps)
+
+the amount of average-damps grows logarithmically with the nth square root.
+
+log
+;Value 5: #[compiled-procedure 5 ("arith" #xcc) #x3 #x35b10]
+
+Use the change of base formula to compute the logarithm of n with respect to the base 2
+
+(define (log2 x)
+  (/ (log x) (log 2)))
+;Value: log2
+
+
