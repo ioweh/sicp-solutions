a0830b78802358c1523271eb26518734fe0cfc09
a <a@bortnikov.com>
2013-07-15 17:09:22 +0400

    Exercise 2.23. The procedure for-each

---
+++ 2.2/2.23.scm
@@ -0,0 +1,31 @@
+
+for-each
+;Value 2: #[compiled-procedure 2 ("list" #x6d) #x3 #x4a750]
+
+(for-each (lambda (x) (newline) (display x))
+	  (list 57 321 88))
+
+57
+321
+88
+;Unspecified return value
+
+(define (for-each proc items)
+  (cond ((null? items)
+	 (newline))
+	(else
+	 (proc (car items))
+	 (for-each proc (cdr items)))))
+;Value: for-each
+
+
+(for-each (lambda (x) (newline) (display x))
+	  (list 57 321 88 true))
+
+57
+321
+88
+#t
+;Unspecified return value
+
+
