bad9a61452e84eb5695f337a17174d7e4b33ca6a
a <a@bortnikov.com>
2013-07-19 00:24:57 +0400

    Exercise 2.25. Counting leaves as an accumulation

---
+++ 2.2/2.35.scm
@@ -0,0 +1,46 @@
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+	  (accumulate op initial (cdr sequence)))))
+;Value: accumulate
+
+(accumulate * 1 (list 1 2 3 4 5))
+;Value: 120
+
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+	((not (pair? x)) 1)
+	(else (+ (count-leaves (car x))
+		 (count-leaves (cdr x))))))
+;Value: count-leaves
+
+(define x (cons (list 1 2) (list 3 4)))
+;Value: x
+
+(count-leaves x)
+;Value: 4
+
+(length x)
+;Value: 3
+
+(define (count-leaves t)
+  (accumulate (lambda (x y) (+ x y)) 0 (map (lambda (sub-tree)
+					      (cond ((pair? sub-tree)
+						     (count-leaves sub-tree))
+						    ((null? sub-tree) 0)
+						    (else 1)))
+					    t)))
+;Value: count-leaves
+
+
+
+(count-leaves x)
+;Value: 4
+
+(count-leaves (list 1 2 3 (list 4 5 6) 7 (list 8 9)))
+;Value: 9
+
+
