a02c45705dbdb9fd0beb74d58106ef39cbda3066
a <a@bortnikov.com>
2013-07-26 21:22:08 +0400

    Exercise 2.45. Defining right-split and up-split in terms of split

---
+++ 2.2/2.45.scm
@@ -0,0 +1,31 @@
+#lang racket
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 0)))
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
+(define (flipped-pairs painter)
+  (let ((combine4 (square-of-four identity flip-vert
+                                  identity flip-vert)))
+    (combine4 painter)))
+
+
+
+
+(define (split pos1 pos2)
+  (lambda (painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller ((split pos1 pos2) painter (- n 1))))
+          (pos1 painter (pos2 smaller smaller))))))
+
+(define right-split (split beside below))
+
+(paint (right-split einstein 3))
+
+(define up-split (split below beside))
+
+(paint (up-split einstein 3))
+
