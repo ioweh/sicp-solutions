9ff82adb3dc7c5226658497f3c13a9f8a57e2e5a
Oleg Ilinets <oleg_ilinets@epam.com>
2014-11-23 22:34:56 +0300

    Added one more solution to 1.7

--- 1.1/1.7.scm
+++ 1.1/1.7.scm
@@ -1,3 +1,5 @@
+1)
+
 (define (sqrt-iter guess x)
   (if (good-enough? guess x)
       guess
@@ -15,3 +17,30 @@
 
 (define (sqrt x)
   (sqrt-iter 1.0 x))
+
+2)
+
+(load "newton-raphson.scm")
+;Loading "newton-raphson.scm"... done
+;Value: sqrt
+
+(sqrt 2)
+;Value: 1.4142156862745097
+
+(define (sqrt x)
+  (sqrt-iter 1.0 (improve 1.0 x) x))
+;Value: sqrt
+
+(define (sqrt-iter guess newguess x)
+  (if (good-enough? guess newguess x)
+      guess
+      (sqrt-iter newguess
+		 (improve newguess x)
+		 x)))
+;Value: sqrt-iter
+
+(define (good-enough? guess newguess x)
+  (< (/ (abs (- guess newguess)) guess) 0.001))
+
+(sqrt 2)
+;Value: 1.4142156862745097
