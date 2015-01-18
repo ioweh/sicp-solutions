23e1caadd4cd9f02f068c66a29640d11fec4b951
Oleg Ilinets <oleg_ilinets@epam.com>
2015-01-18 23:31:33 +0300

    Made the answer to 1.5 concise

--- 1.1/1.5.scm
+++ 1.1/1.5.scm
@@ -5,18 +5,10 @@
       0
       y))
 
-Well
-
-(test 0 (p))
-
-If the interpreter uses applicative-order evaluation, the arguments will be evaluated first and then the operand will be applied to the expression. Zero will evaluate to zero and (p) will call itself recursively without a stop. So it will never halt.
-
-In case of normal-order evaluation the expressions will be fully expanded and only then the arguments will be evaluated. In our case it will look like this:
-
-(test 0 (p))
+In the case of applicative-order evaluation both of the arguments will be evaluated. So the program will never stop executing. While in the case of normal-order evaluation, the program will be executing in the following order:
 
 (if (= 0 0)
     0
     (p))
 
-The result will be zero.
+as we clearly remember, 'if' is the special form, so the second argument will not be evaluated, saving us money spent on the energy for the computer running the program.
