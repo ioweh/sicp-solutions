7f225f66cf4c280d47ce4f2824195c1e303412d8
Oleg Ilinets <oleg_ilinets@epam.com>
2015-01-19 23:31:54 +0300

    Improved solution to 1.6

--- 1.1/1.6.scm
+++ 1.1/1.6.scm
@@ -1,1 +1,8 @@
-The line (sqrt-iter (improve guess x) x) will be executed eternally, as new-if is not a special form and will try to evaluate all the arguments.
+As scheme uses eager evaluation, all the arguments of the new-if procedure should be evaluated. The first argument will evaluate fine, while evaluating the second argument will lead to a disaster! Indeed, let's see ourselves what will happen if we use the new version of if:
+
+new-if (good-enough? guess x)
+    guess				! evaluates fine
+    (sqrt-iter (improve guess x)
+               x)))			! oh, no
+
+Even though the guess will be improved, the procedure sqrt-iter will be called again and again! See what happens if you forgot about the fact that 'if' is actually a special form and doesn't evaluate the arguments if there's no need to.
