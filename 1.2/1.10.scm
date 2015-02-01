326978f3be01eb0f040667731e44ad3757112606
Oleg Ilinets <oleg_ilinets@epam.com>
2015-02-01 22:58:11 +0300

    Improved answer to the Exercise 1.10

--- 1.2/1.10.scm
+++ 1.2/1.10.scm
@@ -71,13 +71,33 @@
 65536
 
 
+(define (f n) (A 0 n))
+This is a pretty trivial example, as the first condition is met right off the bat, therefore the function evaluates to the following:
+
 (define (f n) (A 0 n)) => 2y
+
+
+(define (g n) (A 1 n))
+We clearly see that in this case the expression expands n times, and the result of the last step is 2. Thus we need to multiply 2 n-1 times, or just bring 2 to the power of n, like this:
+
 (define (g n) (A 1 n)) => 2^n 
-(define (h n) (A 2 n)) => 2^2^2^... n times
+
+
+(define (h n) (A 2 n))
+This example is, well, not easy (ok, ok, it's easy, you just need to be careful to details). Let's just see how the function expands, and you'll see how beautiful this exercise is:
+
+(A 2 4)
+(A 1 (A 2 3))
+(A 1 (A 1 (A 2 2)))
+(A 1 (A 1 (A 1 (A 2 1))))
+
+It expands n times before the last expression can be evaluated. Then we decrease n by one and evaluate the last expression to 2.
 
 (A 1 (A 1 (A 1 2)))
-...
-(A 1 (A 1 2^2))
-(A 1 (A 2^2^2))
-...
-2^2^2^2=65536
+
+From the previous example we know that (A 1 n) evaluates to 2^n. In our case it's 2^2. After we evaluated this expression, we need to evaluate the next expression by bringing 2 to the power of 2^2. And we need to do so n-1 times. So, the answer is:
+
+(define (h n) (A 2 n)) => 2^2^2^...  n times
+2^2^2^2
+2^2^4
+2^16
