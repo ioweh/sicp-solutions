f60b8ab208f56ee6ab1798a41c09b5c1f945b48c
Oleg Ilinets <oleg_ilinets@epam.com>
2015-02-04 01:01:19 +0300

    Improved exercise 1.11

--- 1.2/1.11.scm
+++ 1.2/1.11.scm
@@ -1,32 +1,24 @@
 
 (define (f n)
-  (cond ((< n 3) n)
-	(else (+ (f (- n 1)) 
-		 (* 2 (f (- n 2))) 
-		 (* 3 (f (- n 3)))))))
-;Value: f
+  (if (< n 3)
+      n
+      (+ (f (- n 1))
+	 (* 2 (f (- n 2)))
+	 (* 3 (f (- n 3))))))
 
-(f 10)
-;Value: 1892
+(f 6) = 59
 
+(define (f-new n)
+  (f-iter 3 2 1 0 n))
 
-(define (f n)
-  (iter-f 2 1 0 3 n))
-;Value: f
+(define (f-iter counter a b c n)
+  (if (> counter n)
+      a
+      (f-iter (+ counter 1)
+	      (+ a (* 2 b) (* 3 c))
+	      a
+	      b
+	      n)))
 
-
-(define (iter-f f1 f2 f3 counter maxcount)
-  (cond (( or (> counter maxcount) (= counter maxcount)) (+ f1 (* 2 f2) (* 3 f3)))
-	(else (iter-f (+ f1 (* 2 f2) (* 3 f3))
-		      f1
-		      f2
-		      (+ counter 1)
-		      maxcount))))
-;Value: iter-f
-
-(f 10)
-;Value: 1892
-
-
-
+(f-new 6) = 59
 
