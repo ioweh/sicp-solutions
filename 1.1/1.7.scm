b36ec0a640aafac812502349bf68c1c29e48d81e
Oleg Ilinets <oleg_ilinets@epam.com>
2016-03-25 02:46:16 +0300

    Exercise 1.7. Added more explanations. Overengineering is bad, mmkay

--- 1.1/1.7.scm
+++ 1.1/1.7.scm
@@ -18,6 +18,12 @@
 (define (sqrt x)
   (sqrt-iter 1.0 x))
 
+See this community wiki post for details:
+
+http://community.schemewiki.org/?sicp-ex-1.7
+
+"Just using 0.001 is, in effect, doing the same thing as the original program. It basically says 'If the difference between this guess and improved guess is less than 0.0001 in absolute terms (as opposed to percent terms) then stop improving.'  Problem with this is that for really tiny numbers, it is easy for the total difference between guess and improve guess to be less than .0001 and for the program to stop without actually doing anything. For large numbers, it might take forever to get to where guess and improved guess are less than .0001. So the book asks us to stop the program if improved guess is less than a certain PERCENT of guess. And THAT is what this alternative does."
+
 2)
 
 (load "newton-raphson.scm")
