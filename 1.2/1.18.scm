994ad0c08e9292128bc4c4cd6d49dc8c44ba1a7a
Oleg Ilinets <oleg_ilinets@epam.com>
2016-04-19 01:52:44 +0300

    Exercise 1.17. Some editing

--- 1.2/1.18.scm
+++ 1.2/1.18.scm
@@ -6,17 +6,8 @@
   (/ x 2))
 ;value: halve
 
-; let's define an invariant quantity
-; (* iq (* a b)) = const
-; 1-st iteration
-; (* iq (* (double a) (halve b))) = const
-; iq is still 1
-; n-th iteration
-; (* iq (* (double (double ... a)) 1)) = const
-; iq = (* (double (double (double ... a))))
-
 (define (* a b)
-  (cond ((= b 1) a)
+  (cond ((= b 0) 0)
 	((even? b) (* (double a) (halve b)))
 	(else (+ a (* a (- b 1))))))
 ;Value: *
@@ -27,5 +18,21 @@
 (* 4 5)
 ;Value: 20
 
+Note: we use an invariant here:
 
+c + a * b = const
+
+Initially, c equals 0. Let's assume that on the first step we encounter an odd number, and the equation above will look like:
+
+a + a * (b - 1) = const
+
+which is, in other terms:
+
+c1 + a1 * b1 = const
+
+On the last step, the final version will look like the following (as 1 is odd):
+
+cn + 0 = const
+
+And the answer will be readily produced by cn.
 
