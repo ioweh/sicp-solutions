3d131d2d9519d0967687b5c67816ce4e09f36f8f
Oleg Ilinets <oleg.ilinets@gmail.com>
2017-03-25 00:41:19 +0400

    Exercise 1.24. Added the notion on how the time of two different runs of the procedure compares

--- 1.2/1.24.scm
+++ 1.2/1.24.scm
@@ -60,3 +60,46 @@
 1363005552434666078217421284621279933627102780881053358473 *** .00999999999999801
 ;Unspecified return value
 
+
+
+Let's run the test with bigger numbers to get some discernible results.
+
+
+(search-for-primes 10000000000000 10000000000100)
+
+10000000000037 *** 1.375
+10000000000051 *** 1.4079999999999586
+10000000000099 *** 1.393000000000029
+;Unspecified return value
+
+(search-for-primes 10000000000000000 10000000000000100)
+
+10000000000000061 *** 1.8280000000000314
+10000000000000069 *** 1.76400000000001
+10000000000000079 *** 1.843999999999994
+10000000000000099 *** 1.813000000000045
+;Unspecified return value
+
+
+Note that the input increased 1000 times. Now, let's calculate how the amounts of time required for the algorithm execution on two different steps relate to each other.
+
+For the first procedure, which has logarithmic growth, the time is proportional to the log(n), where n is the input parameter. Let's assume that the exact time required is some constant k multiplied by the log(n), or
+
+k log(n)
+
+Now let's consider the second step. The amount of time required is:
+
+k log (1000 n)
+
+We can transform this expression to the following one:
+
+k log 1000 + k log n = 3 k + k log n
+
+After the transformation, we can calculate the ratio of the time required to run the two different procedures:
+
+(3 k + k log n)/(k log n) = (3 + log n)/(log n)
+
+Resolving this equation for n = 10000000000000 we get the ratio that is equal to 16/13 or 1.230769. This means that on the next step the time was increased 1.230769 times in compare with the previous one. Now we can apply this ratio that we calculated to the time that we've got in the first run of the search-for-primes procedure.
+
+By doing so, we receive 1.7097 or some value similar to that. It's easy to note that the actual time required for the second run of the procedure is slightly greater. Most likely, it's related to how the interpreter conducts operations on bigger numbers.
+
