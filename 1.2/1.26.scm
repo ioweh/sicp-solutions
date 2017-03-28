7a1072e786761bb99076779bc6daff5fd37cc137
Oleg Ilinets <oleg.ilinets@gmail.com>
2017-03-29 00:59:50 +0400

    Exercise 1.26. Added a note on time complexity

--- 1.2/1.26.scm
+++ 1.2/1.26.scm
@@ -11,3 +11,8 @@
 
 Clearly, we now call expmod twice. At each computation the amount of steps divides and multiplies by 2.
 The total amount of steps is proportional to exp.
+
+
+A more detailed answer:
+
+At each step of the computation we now double the number of calls to the expmod procedure. The number of computations will be proportional to log N, where N is the input parameter (namely, the positive integer less than n, where n is the supposed to be prime to be tested). Therefore, number of expmod calls will be proportional to 2*2*2*...*2 log N times. Thus, the time complexity will be O(n).
