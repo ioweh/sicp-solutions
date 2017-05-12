2257a59c2e2eb31b51ef6d0867917660f67fd2d7
Oleg Ilinets <oleg.ilinets@gmail.com>
2017-05-13 00:45:09 +0400

    Exercise 1.29. Simpson's Rule

--- 1.3/1.29.scm
+++ 1.3/1.29.scm
@@ -16,18 +16,32 @@
      (/ h 3)))
 ;Value: integral
 
+***
 
+Another solution:
 
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+	 (sum term (next a) next b))))
+;Value: sum
 
+(define (simpson-integral f a b n)
+  (define h (/ (- b a) n))
+  (define (next x) (+ x h))
+  (define (term x)
+    (cond ((or (= x a) (= x b))(f x))
+	  ((even? (/ (- x a) h)) (* 2 (f x)))
+	  (else (* 4 (f x)))))
+  (* (/ h 3) (sum term a next b)))
+;Value: simpson-integral
 
+(define (cube x) (* x x x))
+;Value: cube
 
-
-
-
-
-
-
-
+(simpson-integral cube 0 1 100)
+;Value: 1/4
 
 
 
