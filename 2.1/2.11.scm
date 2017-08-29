8dbc8f4b9ea63369b03dcd1dffb77038ef5653bf
Oleg Ilinets <oleg.ilinets@gmail.com>
2017-08-30 00:47:09 +0400

    Exercise 2.11. Added one more solution

--- 2.1/2.11.scm
+++ 2.1/2.11.scm
@@ -191,3 +191,52 @@ It's time to check the formula.
 (mul-interval-old a b)
 ;Value 29: (8 . 21)
 
+
+
+***
+
+One more solution:
+
+
+(define (mul-interval x y)
+  (cond ((and (< (lower-bound x) 0) (> (upper-bound x) 0)
+	      (< (lower-bound y) 0) (> (upper-bound y) 0))
+	 (make-interval (min (* (lower-bound x) (upper-bound y))
+			     (* (upper-bound x) (lower-bound y)))
+			(max (* (lower-bound x) (lower-bound y))
+			     (* (upper-bound x) (upper-bound y)))))
+	((and (< (lower-bound x) 0) (> (upper-bound x) 0)
+	      (< (lower-bound y) 0) (< (upper-bound y) 0))
+	 (make-interval (* (upper-bound x) (lower-bound y))
+			(* (lower-bound x) (lower-bound y))))
+	((and (< (lower-bound x) 0) (> (upper-bound x) 0)
+	      (> (lower-bound y) 0) (> (upper-bound y) 0))
+	 (make-interval (* (lower-bound x) (upper-bound y))
+			(* (upper-bound x) (upper-bound y))))
+	((and (< (lower-bound x) 0) (< (upper-bound x) 0)
+	      (< (lower-bound y) 0) (> (upper-bound y) 0))
+	 (make-interval (* (lower-bound x) (upper-bound y))
+			(* (lower-bound x) (lower-bound y))))
+	((and (< (lower-bound x) 0) (< (upper-bound x) 0)
+	      (< (lower-bound y) 0) (< (upper-bound y) 0))
+	 (make-interval (* (upper-bound x) (upper-bound y))
+			(* (lower-bound x) (lower-bound y))))
+	((and (< (lower-bound x) 0) (< (upper-bound x) 0)
+	      (> (lower-bound y) 0) (> (upper-bound y) 0))
+	 (make-interval (* (lower-bound x) (upper-bound y))
+			(* (upper-bound x) (lower-bound y))))
+	((and (< (lower-bound y) 0) (> (upper-bound y) 0)
+	      (> (lower-bound x) 0) (> (upper-bound y) 0))
+	 (make-interval (* (upper-bound x) (lower-bound y))
+			(* (upper-bound x) (upper-bound y))))
+	((and (> (lower-bound x) 0) (> (upper-bound x) 0)
+	      (< (lower-bound y) 0) (< (upper-bound y) 0))
+	 (make-interval (* (upper-bound x) (lower-bound y))
+			(* (lower-bound x) (upper-bound y))))
+	((and (> (lower-bound x) 0) (> (upper-bound x) 0)
+	      (> (lower-bound x) 0) (> (upper-bound y) 0))
+	 (make-interval (* (lower-bound x) (lower-bound y))
+			(* (upper-bound x) (upper-bound y))))))
+;Value: mul-interval
+
+
