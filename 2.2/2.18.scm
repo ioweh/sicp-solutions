00e1bfd84cb090d7b50130e306107b6b7f9b1653
Oleg Ilinets <oleg.ilinets@gmail.com>
2017-10-20 01:35:46 +0400

    Exercise 2.18. Optimized the last step

--- 2.2/2.18.scm
+++ 2.2/2.18.scm
@@ -15,5 +15,17 @@
 ;Value 18: (25 16 9 4 1)
 
 
+***
 
 
+(define (reverse items)
+  (define (reverse-iter items reversed-items)
+    (if (null? items)
+	reversed-items
+	(reverse-iter (cdr items) (cons (car items) reversed-items))))
+  (reverse-iter items ()))
+;Value: reverse
+
+
+(reverse (list 1 4 9 16 25))
+;Value 18: (25 16 9 4 1)
