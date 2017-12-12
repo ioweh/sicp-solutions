03d9d613e26ec23aded04cffbee14f77adc70889
Oleg Ilinets <oleg.ilinets@gmail.com>
2017-12-13 02:54:48 +0400

    Exercise 2.29. Added one more soluiton

--- 2.2/2.29.scm
+++ 2.2/2.29.scm
@@ -192,3 +192,141 @@ We need to also change (list? ...) to (pair? ...).
 
 
 
+***
+
+One more solution:
+
+
+(define (make-mobile left right)
+  (list left right))
+;Value: make-mobile
+
+(define (make-branch length structure)
+  (list length structure))
+;Value: make-branch
+
+(define left-branch car)
+;Value: left-branch
+
+(define right-branch cadr)
+;Value: right-branch
+
+(define branch-length car)
+;Value: branch-length
+
+(define branch-structure cadr)
+;Value: branch-structure
+
+(define m1 (make-mobile (make-branch 3 40)
+			(make-branch 4 25)))
+;Value: m1
+
+
+m1
+;Value 19: ((3 . 40) 4 . 25)
+
+(define m2 (make-mobile (make-branch 3 40)
+			(make-branch 4 (make-mobile (make-branch 3 11)
+						    (make-branch 4 12)))))
+;Value: m2
+
+m2
+;Value 20: ((3 . 40) 4 (3 . 11) 4 . 12)
+
+
+(define (balanced? m)
+  (cadr (calc-weight-state m)))
+;Value: balanced?
+
+(define (calc-weight-state m)
+  (let ((weight-state-left (if (pair? (branch-structure (left-branch m)))
+			       (calc-weight-state (branch-structure (left-branch m)))
+			       (list (branch-structure (left-branch m)) #t)))
+	(weight-state-right (if (pair? (branch-structure (right-branch m)))
+				(calc-weight-state (branch-structure (right-branch m)))
+				(list (branch-structure (right-branch m)) #t))))
+    (list (+ (car weight-state-left) (car weight-state-right))
+	  (and (cadr weight-state-left) (cadr weight-state-right) (= (* (branch-length (left-branch m)) (car weight-state-left))
+								     (* (branch-length (right-branch m)) (car weight-state-right)))))))
+;Value: calc-weight-state
+
+
+(calc-weight-state m1)
+;Value 15: (65 #f)
+
+(calc-weight-state m2)
+;Value 16: (63 #f)
+
+(define m3 (make-mobile (make-branch 3 40)
+			(make-branch 2 (make-mobile (make-branch 4 20)
+						    (make-branch 2 40)))))
+;Value: m3
+
+m3
+;Value 21: ((3 . 40) 2 (4 . 20) 2 . 40)
+
+(calc-weight-state m3)
+;Value 18: (100 #t)
+
+(balanced? m3)
+;Value: #t
+
+(balanced? m2)
+;Value: #f
+
+(balanced? m3)
+;Value: #t
+
+(define (make-mobile left right)
+  (cons left right))
+;Value: make-mobile
+
+(define (make-branch length structure)
+  (cons length structure))
+;Value: make-branch
+
+(define left-branch car)
+;Value: left-branch
+
+(define right-branch cdr)
+;Value: right-branch
+
+(define branch-length car)
+;Value: branch-length
+
+(define branch-structure cdr)
+;Value: branch-structure
+
+
+(calc-weight-state m3)
+;Value 22: (100 #t)
+
+
+; only selectors need to be changed
+
+(balanced? m3)
+;Value: #t
+
+(balanced? m2)
+;Value: #f
+
+(define (total-weight m)
+  (define (branch-weight b)
+    (if (not (pair? (branch-structure b)))
+	(branch-structure b)
+	(total-weight (branch-structure b))))
+  (+ (branch-weight (left-branch m))
+     (branch-weight (right-branch m))))
+;Value: total-weight
+
+(total-weight m1)
+;Value: 65
+
+(total-weight m2)
+;Value: 63
+
+(total-weight m3)
+;Value: 100
+
+; indeed, only selectors need to be changed
+
