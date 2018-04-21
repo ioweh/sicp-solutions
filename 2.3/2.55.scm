c3ac8e837088d99e8c29a7c80dca8a1017804861
Oleg Ilinets <oleg.ilinets@gmail.com>
2018-04-22 00:53:39 +0400

    Exercise 2.55. Added a link to the best answer

--- 2.3/2.55.scm
+++ 2.3/2.55.scm
@@ -17,3 +17,20 @@ Test it
 (car (quote (quote abracadabra)))
 ;Value: quote
 
+***
+
+The best answer is given here:
+
+https://stackoverflow.com/questions/16885044/when-does-scheme-evaluate-quote
+
+When scheme evaluates a combination it evaluates the operator. Then it will apply if it's a special form or macro. If not, it will evaluate each operands before applying.
+
+(car (quote (quote abracadabra)))
+
+Can be evaluated like this*
+
+    car evaluates to #<proc car>. since it's a procedure evaluate all operands in any order
+    (quote (quote abracadabra)) is a combination, thus
+        quote is evaluated => #<macro quote> and since its a macro/special form do macro-apply
+        apply-macro (#<macro quote> (quote abracadabra))) => (quote abracadabra)
+    apply-proc (#<proc car> (quote abracadabra)) => quote
