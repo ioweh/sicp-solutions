066b3661b96b1dda709d88d4fb8d2383921e9bb7
Oleg Ilinets <oleg.ilinets@gmail.com>
2022-12-01 02:17:38 +0400

    Exercise 3.4 - call-the-police procedure

--- 3.1/3.3.scm
+++ 3.1/3.3.scm
@@ -1,26 +1,30 @@
-(not (eq? 2 3))
-;Value: #t
-
-
-
 
 
 (define (make-account balance password)
-  (define (withdraw amount)
-    (if (>= balance amount)
-	(begin (set! balance (- balance amount))
-	       balance)
-	"Insufficient funds"))
-  (define (deposit amount)
-    (set! balance (+ balance amount))
-    balance)
-  (define (dispatch p m)
-    (cond ((not (eq? p password)) (error "Incorrect password" p))
-	  ((eq? m 'withdraw) withdraw)
-	  ((eq? m 'deposit) deposit)
-	  (else (error "Unknown request -- MAKE-ACCOUNT"
-		       m))))
-  dispatch)
+  (let ((number-of-calls 0))
+    (define (call-the-cops)
+      "Calling the police!")
+    (define (withdraw amount)
+      (if (>= balance amount)
+	  (begin (set! balance (- balance amount))
+		 balance)
+	  "Insufficient funds"))
+    (define (deposit amount)
+      (set! balance (+ balance amount))
+      balance)
+    (define (dispatch p m)
+      (cond ((not (equal? p password))
+	     (set! number-of-calls (+ number-of-calls 1))
+	     (lambda (any)
+	       (if (>= number-of-calls 7)
+		   (call-the-cops)
+		   "Insufficient funds")))
+	    ((eq? number-of-calls 7) (call-the-cops))
+	    ((eq? m 'withdraw) withdraw)
+	    ((eq? m 'deposit) deposit)
+	    (else (error "Unknown request -- MAKE-ACCOUNT"
+			 m))))
+    dispatch))
 ;Value: make-account
 
 (define acc (make-account 100 'vika))
@@ -30,10 +34,17 @@
 ;Value: 60
 
 ((acc 'oleg 'withdraw) 20)
-;Incorrect password oleg
-;To continue, call RESTART with an option number:
-; (RESTART 3) => Return to read-eval-print level 3.
-; (RESTART 2) => Return to read-eval-print level 2.
-; (RESTART 1) => Return to read-eval-print level 1.
-;Start debugger? (y or n): n
+;Value: "Calling the police!"
+
+;Value: "Insufficient funds"
+
+;Value: "Insufficient funds"
+
+;Value: "Insufficient funds"
+
+;Value: "Insufficient funds"
+
+;Value: "Insufficient funds"
+
+;Value: "Insufficient funds"
 
